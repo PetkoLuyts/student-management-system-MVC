0be202c0a055f5009986298ceabb2c08a39bc285
petko.lyutskanov <simona2000petko@abv.bg>
2023-01-15 01:28:17 +0200

    Initial commit

---
+++ src/test/java/com/example/studentmanagementsystemmvc/StudentManagementSystemMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagementsystemmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
